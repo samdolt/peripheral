21f3f7ef9eac6f5831c3bf16749b5ccf17eb8b82
Samuel Dolt <samuel@dolt.ch>
2016-10-18 18:27:13 +0200

    Update

--- src/lib.rs
+++ src/lib.rs
@@ -1,35 +1,67 @@
-#![no_std]
+#![cfg_attr(not(feature = "use_std"), no_std)]
 
+pub enum Error {
+    Unavailable,
+    Invalid,
+    Unexpected,
+    IOError,
+}
+
+#[cfg(feature = "use_std")]
+pub type Result<T> = ::std::result::Result<T, Error>;
+
+#[cfg(not(feature = "use_std"))]
+pub type Result<T> = ::core::result::Result<T, Error>;
+
+pub enum InputMode {
+    Default,
+    Floating,
+    PulledUp,
+    PulledDown,
+    Unknow,
+}
+
+pub enum OutputMode {
+    Default,
+    PushPull,
+    OpenDrain,
+    OpenSource,
+    Unknow,
+}
+
+/// Generic Digital Input trait
 pub trait DigitalIn {
-    fn read(&self) -> Option<bool>;
-    fn set_input_mode(&mut self);
-    fn set_to_input(&mut self) -> Option<()>;
+    /// Read the digital Input
+    ///
+    /// # Error
+    ///
+    /// On some system, reading a pin can fail (Eg. on Linux)
+    fn read(&self) -> Result<bool>;
+    fn set_input_mode(&mut self) -> Result<()>;
+    fn set_to_input(&mut self) -> Result<()>;
 }
 
 pub trait DigitalOut {
-    fn write(&mut self, value: bool) -> Option<bool>;
-    fn set_output_mode(&mut self);
-    fn set_low(&mut self);
-    fn set_high(&mut self);
-    fn set_to_output(&mut self) -> Option<()>;
+    fn write(&mut self, value: bool) -> Result<bool>;
+    fn set_output_mode(&mut self) -> Result<()>;
+    fn set_low(&mut self) -> Result<()>;
+    fn set_high(&mut self) -> Result<()>;
+    fn set_to_output(&mut self) -> Result<()>;
 }
 
 pub trait DigitalIO: DigitalIn + DigitalOut {
-    fn set_direction(&mut self, value: bool) -> Option<()> {
+    fn set_direction(&mut self, value: bool) -> Result<()> {
         if value {
             self.set_to_input()
         } else {
             self.set_to_output()
         }
-
     }
 }
 
-pub trait AnalogIn{}
-pub trait AnalogOut{}
-pub trait AnalogIO{}
-
-
+pub trait AnalogIn {}
+pub trait AnalogOut {}
+pub trait AnalogIO {}
 
 #[cfg(test)]
 mod tests {
