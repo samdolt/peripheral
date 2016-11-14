7f34a3c10eb97d2960ef9ce3b359f3396e16ade8
Samuel Dolt <samuel@dolt.ch>
2016-11-14 21:59:43 +0100

    Implement Display, Error and Debug for Error

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,28 @@
- 
+#[derive(Debug)] 
 pub enum Error {
     Unavailable,
     Invalid,
     Unexpected,
     IOError,
+}
+
+use std::error;
+use std::fmt;
+
+impl fmt::Display for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            write!(f, "Error")
+    }
+}
+
+impl error::Error for Error {
+    fn description(&self) -> &str {
+        "Descr"
+    }
+
+    fn cause(&self) -> Option<&error::Error> {
+        None
+    }
 }
 
 #[cfg(feature = "use_std")]
